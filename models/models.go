c7bafe2e5ea36eac6d2ae418373fed4045e4ac45
Steven Ferrer <steven.r.ferrer@gmail.com>
2017-10-07 02:41:55 +0800

    added models and store

---
+++ models/models.go
@@ -0,0 +1,77 @@
+package models
+
+//User represents an application user
+type User struct {
+	ID         int64
+	Email      string
+	Password   string
+	FirstName  string
+	MiddleName string
+	LastName   string
+}
+
+//UserFilter is used for filtering results
+type UserFilter func(*UserFilterConfig) error
+
+//UserFilterConfig is the config for filtering results
+type UserFilterConfig struct {
+	Limit      int64
+	ID         int64
+	Email      string
+	FirstName  string
+	MiddleName string
+	LastName   string
+}
+
+func UserLimitFilter(limit int64) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.Limit = limit
+		return nil
+	}
+}
+
+func UserIDFilter(id int64) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.ID = id
+		return nil
+	}
+}
+
+func UserEmailFilter(email string) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.Email = email
+		return nil
+	}
+}
+
+func UserFirstNameFilter(firstName string) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.FirstName = firstName
+		return nil
+	}
+}
+
+func UserMiddleNameFilter(middleName string) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.MiddleName = middleName
+		return nil
+	}
+}
+
+func UserLastNameFilter(lastName string) UserFilter {
+	return func(fc *UserFilterConfig) error {
+		fc.LastName = lastName
+		return nil
+	}
+}
+
+//UserStorer represents a user store
+//this can be used to store user on different
+//data store
+type UserStorer interface {
+	List(...UserFilter) ([]User, error)
+	Get(...UserFilter) (User, error)
+	Create(User) error
+	Update(User) error
+	Delete(...UserFilter) error
+}
---
+++ store/store.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"github.com/jmoiron/sqlx"
+
+	"github.com/steven-ferrer/go-db-models/models"
+)
+
+//UserStore implements models.UserStorer
+type UserStore struct {
+	db *sqlx.DB
+}
+
+func (cs *UserStore) List(filters ...models.UserFilter) ([]models.User, error) {
+	return nil, nil
+}
+
+func (cs *UserStore) Get(filters ...models.UserFilter) (models.User, error) {
+	return models.User{}, nil
+}
+
+func (cs *UserStore) Create(User models.User) error {
+	return nil
+}
+
+func (cs *UserStore) Update(User models.User) error {
+	return nil
+}
+
+func (cs *UserStore) Delete(filters ...models.UserFilter) error {
+	return nil
+}
+
+func NewUserStore(db *sqlx.DB) models.UserStorer {
+	return &UserStore{db}
+}
